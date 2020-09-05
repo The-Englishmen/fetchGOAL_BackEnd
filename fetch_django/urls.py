c6670e0a4c3a2ce920ea677ef7b40c40d46b5388
AnylPascal <info@anylpascal.com>
2020-09-05 11:09:18 -0400

    bug fixes

--- fetch_django/urls.py
+++ fetch_django/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
