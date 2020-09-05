e0974798658cd517a3c90d27d74a532831b4cbe2
AnylPascal <info@anylpascal.com>
2020-09-05 12:12:10 -0400

    removed issues with Pipfile.lock

--- fetch_django/urls.py
+++ fetch_django/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
@@ -20,4 +22,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('app.urls')),
 ]
