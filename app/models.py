750abaa1871ead01e4f1d6dc7ddf832f765ee034
AnylPascal <info@anylpascal.com>
2020-09-09 11:55:57 -0400

    database restuctured/bugs cleared

--- app/models.py
+++ app/models.py
@@ -13,7 +13,6 @@ class Federation(models.Model):
 
 class League(models.Model):
     name = models.CharField(max_length=100)
-    federation = models.ForeignKey(Federation, on_delete=models.CASCADE, related_name='leagues', null=True)
     photo_url = models.TextField()
 
     def __str__(self):
@@ -23,8 +22,9 @@ class League(models.Model):
 class Team(models.Model):
     name = models.CharField(max_length=100)
     league = models.ForeignKey(League, on_delete=models.CASCADE, related_name='teams')
+    federation = models.ForeignKey(Federation, on_delete=models.CASCADE, related_name='leagues')
     photo_url = models.TextField(default=None)
 
 
     def __str__(self):
-        return f'{self.name},{self.league},{self.photo_url}'
+        return f'{self.name},{self.league},{self.photo_url},{self.federation}'
