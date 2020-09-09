b49118c3b29eb925e3ad8ea4c25246c3c742b281
AnylPascal <info@anylpascal.com>
2020-09-09 17:14:35 -0400

    updates

--- app/models.py
+++ app/models.py
@@ -9,22 +9,23 @@ class Federation(models.Model):
     photo_url = models.TextField()
 
     def __str__(self):
-        return f'{self.name},{self.photo_url}'
+        return self.name
 
 class League(models.Model):
     name = models.CharField(max_length=100)
     photo_url = models.TextField()
+    federation = models.ForeignKey(Federation, on_delete=models.CASCADE, related_name='leagues')
 
     def __str__(self):
-        return f'{self.name},{self.federation},{self.photo_url}'
+        return self.name
 
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
     league = models.ForeignKey(League, on_delete=models.CASCADE, related_name='teams')
-    federation = models.ForeignKey(Federation, on_delete=models.CASCADE, related_name='leagues')
-    photo_url = models.TextField(default=None)
+    federation = models.ForeignKey(Federation, on_delete=models.CASCADE, related_name='teams')
+    photo_url = models.TextField()
 
 
     def __str__(self):
-        return f'{self.name},{self.league},{self.photo_url},{self.federation}'
+        return self.name
